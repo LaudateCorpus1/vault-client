9c72b89aa246540781fa5631d51fcfd28828fb73
replicant0wnz <replicant@dallaslamers.org>
2021-07-07 11:13:01 -0500

    Testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,6 @@ source /bishy.bash
 get_longOpt $@
 
 echo "::set-output name=vault_addr_out::$addr"
-echo "version 0.15"
+echo "version 0.16"
 echo "---------"
-echo $1
-echo "---------"
-echo $2
-echo "---------"
-echo $3
+echo $secrets

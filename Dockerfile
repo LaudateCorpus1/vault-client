ee0b9621c546e7248f0735f94cda7d4301efba70
replicant0wnz <replicant@dallaslamers.org>
2021-07-07 10:52:25 -0500

    Testing

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,1 @@
-FROM gcr.io/viant-images/actions/vault-client:0.14
+FROM gcr.io/viant-images/actions/vault-client:0.15
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,10 @@ source /bishy.bash
 get_longOpt $@
 
 echo "::set-output name=vault_addr_out::$addr"
-echo "version 0.14"
-echo "$@"
+echo "version 0.15"
+echo "---------"
+echo $1
+echo "---------"
+echo $2
+echo "---------"
+echo $3
